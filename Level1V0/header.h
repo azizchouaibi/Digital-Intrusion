91d525aff10c40df988fd95d604cd6d03bff0c54
azizchouaibi <mohamedazizchouaibi@gmail.com>
2024-03-26 15:13:54 +0100

    (tache background tache perso)

---
+++ Level1V0/header.h
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+#include <stdbool.h>
+#include <time.h>
+#include "back.h"
+#define ground 500
+typedef struct {
+	
+	SDL_Rect posinit;
+	SDL_Surface* texture[15];
+	int textcourant;
+	// proprietes 
+	int num_hearts;
+	SDL_Surface * vie[3];
+	SDL_Rect posvie;
+	double acc;
+	SDL_Surface * dmg;
+
+}Person;
+
+
+Person  InitPerso(Person  player );
+
+void afficherPerso(Person * player, SDL_Surface * ecran);
+void deplacerPerso(Person * player, int direction, int *currenttext);
+void animerPerso (Person * player , SDL_Surface * ecran, int *compt);
+void jumpPar(Person *player);
+void jumpPerso ( Person * player , int direction);
+void afficher_score(int score ,  SDL_Rect * PosTxt, SDL_Surface ** SurfText , TTF_Font * font , SDL_Color txtCoul);
+
+
+
+typedef struct {
+	SDL_Rect posarr;
+	SDL_Surface * back[3];
+	int indback;
+} Backg;
+
+
+Backg initbackground(Backg  Back);
+
+
+
+ 
+ 
+
+ 
+ 	
+	
